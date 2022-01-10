bfa99a6c4e381ed58a19405f623e85820de73248
gerszcitoo <germands2013@gmail.com>
2022-01-10 02:03:54 -0300

    mobile navbar toggle WIP

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ $('.mobile-nav').click(function() {
     $(".nav").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
 });
+const navToggle = document.querySelector(".mobile-nav");
+const primaryNav = document.querySelector(".primary-nav");
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+    }
+});
