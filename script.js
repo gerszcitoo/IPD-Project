e2bc6ab01af20169748bea4a7f931a9c9b1637c6
gerszcitoo <germands2013@gmail.com>
2022-01-11 21:45:04 -0300

    navbar functionality finished

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ $('.mobile-nav').click(function() {
     $(".nav").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
 });
+
 const navToggle = document.querySelector(".mobile-nav");
 const primaryNav = document.querySelector(".primary-nav");
 navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible");
+    const aria = primaryNav.getAttribute("aria-expanded")
     if (visibility === "false") {
         primaryNav.setAttribute("data-visible", true);
+        primaryNav.setAttribute("aria-expanded", true);
     } else if (visibility === "true") {
         primaryNav.setAttribute("data-visible", false);
+        primaryNav.setAttribute("aria-expanded", false);
     }
 });
