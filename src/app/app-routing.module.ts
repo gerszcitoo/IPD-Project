adfa1e13400389b358f14391aec5d4ec98eaa940
gerszcitoo <germands2013@gmail.com>
2022-02-26 20:31:31 -0300

    Angular routing problem solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // { path: 'home', component: AppComponent },
+  { path: 'home', component: HomeComponent },
   {path: "", redirectTo: "home", pathMatch: "full"},
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -14,7 +15,8 @@ import { ContactComponent } from './contact/contact.component';
     AppComponent,
     NavbarComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
